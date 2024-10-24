87b8dafdc6f6b2f331209cbeee8794c81a619a83
Danila <danila260390@gmail.com>
2024-10-24 16:28:37 -0400

    add fibonacci function

--- test.py
+++ test.py
@@ -1,3 +1,28 @@
-print('hello world')
+from typing import Union
 
-print(2)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/fibonacci/{n}")
+def fib(n: int):
+    '''возвращает n-e число из последовательности Фибоначчи'''
+    if type(n) != 'int' and n < 0:
+        raise TypeError('Введите натуральное число')
+    else:
+        a, b = 0, 1
+        while n > 0:
+            a, b = b, a + b
+            n -= 1
+    return a
